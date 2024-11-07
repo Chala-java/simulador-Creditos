434c924a2d09df00f2db3057d8cc9807a947c247
JuanEscobar10 <desarrollo@academicos.cesde.edu.co>
2024-11-07 15:31:49 -0500

    Aqui se trabajo expresiones regulares y como llamar expor e import

--- views/public/js/script.js
+++ views/public/js/script.js
@@ -1,65 +1,42 @@
-// function Persona(nombre, edad, estatura, color) {
-//     this.nombre = nombre
-//     this.edad = edad
-//     this.estatura = estatura
-//     this.color = color
-//     this.moverse = function (nombre) {
-//         this.nombre = nombre
-//         console.log(nombre + ' Se movió')
-//     }
-//     this.expresarse = function () {
-//         console.log('Se expresó...')
-//     }
-// }
-// let personaUno = new Persona('Laura', 18, 1.55, 'Blanco')
-// let personaDos = new Persona('Daniel', 18, 1.74, 'Blanco')
-// let personaTres = new Persona('Anyis', 18, 1.60, 'Blanco')
-// personaUno.moverse()
-// personaDos.moverse()
-// personaTres.moverse()
-// console.log(personaUno, personaDos, personaTres)
+import { expresionesRegulares } from "../../../helpers/expresiones.js";
 
-// let persona = {
-//     nombre: 'Juan Felipe',
-//     apellido: 'Niño',
-//     edad: 19,
-//     moverse: function () {
-//         console.log('Se esta moviendo...')
-//     },
-//     direccion: {
-//         calle: 23,
-//         carrera: 23,
-//         interior: 20
-//     }
-// }
 
-document.querySelector('#btnFormRegistro').addEventListener('click', function () {
-    document.querySelector('#form-login').style.display = 'flex'
-    document.querySelector('#form-register').style.display = 'none'
-})
-document.querySelector('#btnFormLogin').addEventListener('click', function () {
-    document.querySelector('#form-login').style.display = 'none'
-    document.querySelector('#form-register').style.display = 'flex'
-})
-let inputs = document.getElementsByTagName('input')
+
+      
+      document
+      .querySelector("#btnFormRegistro")
+      .addEventListener("click", function () {
+        document.querySelector("#form-login").style.display = "flex";
+        document.querySelector("#form-register").style.display = "none";
+  });
+document.querySelector("#btnFormLogin").addEventListener("click", function () {
+  document.querySelector("#form-login").style.display = "none";
+  document.querySelector("#form-register").style.display = "flex";
+});
+let inputs = document.getElementsByTagName("input");
 for (let index = 0; index < inputs.length; index++) {
-    inputs[index].addEventListener('keyup', validarFormulario)
+  inputs[index].addEventListener("keyup", validarFormulario);
 }
 function validarFormulario(e) {
-    switch (e.target.name) {
-        case 'login-usuario':
-            console.log('Este es el input de login usuario')
-            break;
+  switch (e.target.name) {
+    case "register-nombre":
+      if(expresionesRegulares.nombre.test(e.target.value)){
+        console.log('Paso la validacion');
+        
+      }else{
+        console.log('El campo solo permite letra en mayúscula');
+        
+      }
 
-        default:
-            break;
-    }
+      
+      break;
+
+    default:
+      break;
+  }
 }
 
 // console.log(document.querySelectorAll('input'))
-
-
-
 
 // let arreglo = [1, 'jaime', true, function () { }, { nombre: 'jaime' }, []]
 // console.log(arreglo)
